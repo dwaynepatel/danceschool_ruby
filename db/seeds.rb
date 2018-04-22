49b1fef550b8a892055ec606910f978da32f8d8d
EC2 Default User <ec2-user@ip-172-31-58-221.ec2.internal>
2018-04-22 22:22:53 +0000

    added seeds for products and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Danceclass.create(title: 'Ballroom', description: 'Ballroom classes for all', price:  20, image_url: 'ballroom.jpg', category: 'weekly')
+Danceclass.create(title: 'Traditional', description: 'irish dancing', price:  35, image_url: 'trad.jpg', category: 'weekly')
+Danceclass.create(title: 'Latin', description: 'time to dance', price:  50, image_url: 'latin.jpg', category: 'weekly')
+Danceclass.create(title: 'disco', description: 'disco dancing', price:  40, image_url: 'disco.jpg', category: 'weekly')
+Danceclass.create(title: 'disco', description: 'disco dancing', price:  40, image_url: 'disco.jpg', category: 'weekly')
+Danceclass.create(title: 'Latin', description: 'time to dance', price:  50, image_url: 'latin.jpg', category: 'weekly')
+Danceclass.create(title: 'Ballroom', description: 'Ballroom classes for all', price:  20, image_url: 'ballroom.jpg', category: 'weekly')
+Danceclass.create(title: 'Traditional', description: 'irish dancing', price:  35, image_url: 'trad.jpg', category: 'weekly')
+user = User.new
+user.email = 'user@user.com'
+user.password = 'password'
+user.password_confirmation = 'password'
+user.save!
+
+user = User.new
+user.email = 'admin@admin.com'
+user.password = 'password'
+user.password_confirmation = 'password'
+user.admin = true
+user.save!
