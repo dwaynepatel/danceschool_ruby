eca105be852ff7264f1dee122284429ac30f362c
EC2 Default User <ec2-user@ip-172-31-58-221.ec2.internal>
2018-04-29 19:27:16 +0000

    stripe

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 gem 'devise'
 gem 'bootstrap', '~> 4.0'
 gem 'devise-bootstrap-views', '~> 0.0.7'
+gem 'stripe'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'cart/index'
 
   resources :danceclasses
+  resources :charges
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
@@ -14,5 +15,6 @@ Rails.application.routes.draw do
   get '/cart', to: 'cart#index'
    get '/cart/:id', to: 'cart#add'
    get '/cart/remove/:id', to: 'cart#remove'
+   
 
 end
